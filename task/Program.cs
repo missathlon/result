d9d81b0cdb5a1d8fba605d53eb04b26c0a72f50b
Ekaterina <missathlon08@gmail.com>
2022-10-13 00:58:56 +0300

    add Solved Task

--- task/Program.cs
+++ task/Program.cs
@@ -1,3 +1,23 @@
-﻿
+﻿string[] SelectWords(string[] array, int n)
+{
+    int count=0;    
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= n)
+        {   
+            array[count] = array[i];  
+            count++;          
+        }
+    }
+    Array.Resize(ref array, count);
+    return array;
+}
+
+void Print(string[] array)
+{
+    Console.Write("[" + string.Join(", ", array) + "]");
+    Console.WriteLine();
+}
 
 string[] words = { "privet", ":))", "O_o", "Hiiiiii", "computer", "science", "hh" };
+Print(SelectWords(words,3));
